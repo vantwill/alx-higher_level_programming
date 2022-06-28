f111f3aa5e8855fa7c33486edb539568ae2d2bc7
vantwill <vtwill41@gmail.com>
2022-06-28 15:45:22 -0700

    commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-for k in range(10):
-
-    for j in range(k + 1, 10):
+for i in range(10):
+    for j in range(i + 1, 10):
         print(
-            "{}{}".format(k, j),
-
-            end=", " if int(str(k) + str(j)) < 89 else "\n"
+            "{}{}".format(i, j),
+            end=", " if int(str(i) + str(j)) < 89 else "\n"
             )
