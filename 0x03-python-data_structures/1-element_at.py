9857130933fb71c0249750267cbfecb91e420f5c
vantwill <vtwill41@gmail.com>
2022-07-04 13:31:45 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# 2-replace_in_list.py
-def replace_in_list(my_list, idx, element):
-    """Replace an element of a list at a specific position."""
-    if idx >= 0 and idx < len(my_list):
-        my_list[idx] = element
-    return (my_list)
+def element_at(my_list, idx):
+    """Retrive an element from a list."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
