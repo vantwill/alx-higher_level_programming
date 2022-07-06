7569397c167884f8e3bb37750eed8c2ae79228da
vantwill <vtwill41@gmail.com>
2022-07-06 15:22:25 -0700

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # 2-args.py
+
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
+
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
