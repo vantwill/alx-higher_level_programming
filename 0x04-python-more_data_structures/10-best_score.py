afbb339d326c57a601ebb12b4ff281df8f01aaa7
vantwill <vtwill41@gmail.com>
2022-07-06 14:17:43 -0700

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 10-best_score.py
+def best_score(a_dictionary):
+    """Returns a key with the biggest integer value."""
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for t, g in a_dictionary.items():
+        if v > big:
+            big = g
+            ret = t
+    return (ret)
