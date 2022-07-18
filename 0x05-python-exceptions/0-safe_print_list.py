cb8d6ef549bc96e844754478f360e06f53dba284
vantwill <vtwill41@gmail.com>
2022-07-18 05:12:00 -0700

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for t in range(x):
+        try:
+            print("{:d}".format(my_list[t]), end="")
+            counter += 1
+        except IndexError:
+            break
+    print()
+    return counter
