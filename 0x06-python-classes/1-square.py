82540ca983a51a16e03e5b54e5fd4aea6bb5c71f
vantwill <vtwill41@gmail.com>
2022-07-19 09:44:38 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"0-square.py define"
+
+
+class Square:
+    """Class of square
+    """
+
+    def __init__(self, size):
+        """Def
+        """
+        self.__size = size
