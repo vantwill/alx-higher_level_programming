7980619a4fea1c32914e8cbc680aa085d7aabe7e
vantwill <vtwill41@gmail.com>
2022-10-20 11:40:02 -0700

    commit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-# finds peak
+""" Finds a peak in a list of unsorted integers
+"""
 
-
-
-def search(lo, h, ints):
-    mid = (lo + h) // 2
-    if lo == h:
-        return ints[h]
-    if ints[mid] < ints[mid + 1]:
-        return(search(mid + 1, h, ints))
-    return(search(lo, mid, ints))
 
 def find_peak(list_of_integers):
-    if not list_of_integers:
-        return
-    return(search(0, len(list_of_integers) - 1, list_of_integers))
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
+    """
+    size = len(list_of_integers)
+    midl_e = size
+    midl = size // 2
+
+    if size == 0:
+        return None
+
+    while True:
+        midl_e = mid_e // 2
+        if (midl < size - 1 and
+                list_of_integers[midl] < list_of_integers[midl + 1]):
+            if midl_e // 2 == 0:
+                midl_e = 2
+            midl = midl + midl_e // 2
+        elif midl_e > 0 and list_of_integers[midl] < list_of_integers[midl - 1]:
+            if midl_e // 2 == 0:
+                midl_e = 2
+            midl = midl - midl_e // 2
+        else:
+            return list_of_integers[midl]
