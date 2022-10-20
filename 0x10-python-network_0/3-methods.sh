710957c02695d66de80abd0ddfaac67a15857d82
vantwill <vtwill41@gmail.com>
2022-10-20 10:47:48 -0700

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | grep "Allow: " | sed 's/Allow: //'
+curl -sI "$1" | grep -i Allow | cut -d ' ' -f2-
