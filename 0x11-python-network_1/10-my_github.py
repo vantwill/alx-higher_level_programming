3ffb0661c7ec2286cbe61b4e87ba38c3bd967e98
vantwill <vtwill41@gmail.com>
2022-10-21 13:22:31 -0700

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-# github stuff
+'''
+use the GitHub API to display your id
+'''
 import requests
 from sys import argv
-try:
-    r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
-    print(r.json().get('id'))
-except:
-    print("None")
+if __name__ == "__main__":
+    respo = requests.get('https://api.github.com/user',
+                            auth=(argv[1], argv[2]))
+    if "json" not in respo.headers.get('content-type'):
+        print("Not a valid JSON")
+    else:
+        respo = respo.json()
+        print(respo.get('id'))
