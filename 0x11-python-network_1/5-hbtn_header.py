7834df9e03e655372a8d90e74d1499fbd53e37c2
vantwill <vtwill41@gmail.com>
2022-10-21 12:48:13 -0700

    commit

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id
+send request to custom URL and display value of X-Request-Id
 '''
 import requests
 from sys import argv
