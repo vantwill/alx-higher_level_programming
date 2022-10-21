ef79d7bd5b3e0983dd5a544a62c867412ac07356
vantwill <vtwill41@gmail.com>
2022-10-21 12:57:25 -0700

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,5 +5,5 @@ send POST request to custom URL with email as a parameter
 import requests
 from sys import argv
 if __name__ == "__main__":
-    respns = requests.post(argv[1], data={'email': argv[2]})
-    print(respns.text)
+    respons = requests.post(argv[1], data={'email': argv[2]})
+    print(respons.text)
