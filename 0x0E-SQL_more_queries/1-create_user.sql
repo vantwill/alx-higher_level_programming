9c79e8c5c40094b4500555f744e84198342bb70e
vantwill <vtwill41@gmail.com>
2022-10-22 05:18:03 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- creates the database hbtn_0d_1
-CREATE DATABASE IF NOT EXISTS hbtn_0d_1;
-GRANT SELECT ON hbtn_0d_1.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
