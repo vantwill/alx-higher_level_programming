7e7a0f9864c6e7a3b029d9a86e5bb650dd7c867e
vantwill <vtwill41@gmail.com>
2022-10-22 01:23:36 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- creates the database hbtn_0d_2
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- creates the database hbtn_0d_1
+CREATE DATABASE IF NOT EXISTS hbtn_0d_1;
+GRANT SELECT ON hbtn_0d_1.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
