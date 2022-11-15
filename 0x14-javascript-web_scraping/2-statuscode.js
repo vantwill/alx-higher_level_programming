353e81f2d003412dff8a557d6f5f06da910bfa63
vantwill <vtwill41@gmail.com>
2022-11-15 19:52:38 +0000

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
+
+request(process.argv[2], function (error, response) {
+  if (error) {
+    throw error;
+  }
+  console.log('code: ' + response.statusCode);
 });
