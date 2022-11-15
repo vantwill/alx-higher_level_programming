0eedfdda868155419fd931d4577baf7add9efe85
vantwill <vtwill41@gmail.com>
2022-11-15 19:12:40 +0000

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,24 +1,5 @@
-#!/usr/bin/nod
+#!/usr/bin/node
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
-  if (err) {
-    return console.log(err);
-  }
-  process.stdout.write(data);
-  // console.log(data);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
-
-/*
-const fs = require('fs');
-
-try {
-  const data = fs.readFileSync(process.argv[2], 'utf8');
-  process.stdout.write(data);
-  // console.log(data);
-  // console.log(data.toString());
-} catch (err);
-  console.log(err);
-  console.error(err);
-}
-*/
